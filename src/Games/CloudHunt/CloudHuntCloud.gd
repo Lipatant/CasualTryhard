4ff65abe9ff626a264abc6efc321486d75d6c45d
Lipatant <90960408+Lipatant@users.noreply.github.com>
2023-12-09 17:20:18 +0100

    Revert "Feature/game/adding fishdertale"

--- src/Games/CloudHunt/CloudHuntCloud.gd
+++
@@ -1,6 +0,0 @@
-extends Area2D
-class_name CloudHuntCloud
-
-@export var collision_shape : CollisionShape2D
-
-var game : Game
--- src/Games/CloudHunt/Sun.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-@export var rotation_delta : float = 0.3
-
-func _process(_delta: float) -> void:
-	rotation = rotation + (rotation_delta * _delta)
