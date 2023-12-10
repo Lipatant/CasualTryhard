d523f6cd24f3118930b30d90ad18ff79c7be9b81
Lipatant <virgile.berrier@gmail.com>
2023-12-10 15:09:26 +0100

    feat: reworking zombie sprites

--- src/Games/ZombieHunt/ZombieHuntZombie.gd
+++ src/Games/ZombieHunt/ZombieHuntZombie.gd
@@ -2,9 +2,13 @@ extends Area2D
 class_name ZombieHuntZombie
 
 @export var collision_shape : CollisionShape2D
+@export var sprite : Sprite2D
 
 var game : Game
 var die : bool = false
+
+func _ready() -> void:
+	if sprite: sprite.frame = randi() % (sprite.hframes * sprite.vframes)
 
 func kill():
 	queue_free()
