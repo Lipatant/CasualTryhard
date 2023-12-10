1a28c768cf4d703e90a1cbb40cc759fb7d472ba3
Lipatant <virgile.berrier@gmail.com>
2023-12-10 14:48:59 +0100

    fix: fixing warning and removing debug print

--- src/Games/ZombieHunt/ZombieHunt.gd
+++ src/Games/ZombieHunt/ZombieHunt.gd
@@ -27,18 +27,12 @@ func _process(_delta):
 func _new_click_input(position : Vector2) -> void:
 	if game_state != State.PLAYING or !zombie_container or !weapon: return
 	weapon.shoot()
-	var zombie_hit : int = 0
 	var zombie_rect : Rect2
 	for zombie : ZombieHuntZombie in zombie_container.get_children():
 		if !zombie.collision_shape or !zombie.collision_shape.shape: continue
 		zombie_rect = zombie.collision_shape.shape.get_rect()
-		print("-----------------")
-		print("zombie size : ", zombie_rect.size.x)
-		print("zombie position : ", zombie.collision_shape.global_position.x)
-		print("clic position : ", position.x)
 		if position.x >= zombie.collision_shape.global_position.x - zombie_rect.size.x / 2 and position.x <= zombie.collision_shape.global_position.x + zombie_rect.size.x / 2:
 			zombie_count -= 1
-			zombie_hit += 1
 			zombie.kill()
 			continue
 	if zombie_count < 1:
