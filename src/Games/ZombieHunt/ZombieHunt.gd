9673c03e3b24a9576965495241351b620233d8aa
Skaahn <stanislas.commenge@epitech.eu>
2023-12-10 14:19:56 +0100

    feat: playable zombie hunt

--- src/Games/ZombieHunt/ZombieHunt.gd
+++ src/Games/ZombieHunt/ZombieHunt.gd
@@ -3,11 +3,10 @@ extends Game
 # EXPORT #
 
 @export var zombie_container : Node
-
+@export var weapon : ZombieHuntWeapon
 # ONREADIES #
 
 @onready var zombie_resource : Resource = preload("res://src/Games/ZombieHunt/ZombieHuntZombie.tscn")
-
 # OTHER VARIABLES #
 
 var zombie_count : int = 0
@@ -19,20 +18,28 @@ func _ready() -> void:
 		if child is ZombieHuntZombie:
 			zombie_count += 1
 
+func _process(_delta):
+	if !zombie_container: return
+	for zombie : ZombieHuntZombie in zombie_container.get_children():
+		if zombie.position.y > 500:
+			game_end.emit(false)
+
 func _new_click_input(position : Vector2) -> void:
-	if game_state != State.PLAYING or !zombie_container: return
+	if game_state != State.PLAYING or !zombie_container or !weapon: return
+	weapon.shoot()
 	var zombie_hit : int = 0
 	var zombie_rect : Rect2
 	for zombie : ZombieHuntZombie in zombie_container.get_children():
 		if !zombie.collision_shape or !zombie.collision_shape.shape: continue
 		zombie_rect = zombie.collision_shape.shape.get_rect()
-		if !Rect2(zombie.collision_shape.global_position + zombie_rect.position, zombie_rect.size).has_point(position):
+		print("-----------------")
+		print("zombie size : ", zombie_rect.size.x)
+		print("zombie position : ", zombie.collision_shape.global_position.x)
+		print("clic position : ", position.x)
+		if position.x >= zombie.collision_shape.global_position.x - zombie_rect.size.x / 2 and position.x <= zombie.collision_shape.global_position.x + zombie_rect.size.x / 2:
+			zombie_count -= 1
+			zombie_hit += 1
+			zombie.kill()
 			continue
-		zombie_count -= 1
-		zombie_hit += 1
-		zombie.queue_free()
-		zombie_container.remove_child(zombie)
 	if zombie_count < 1:
 		game_end.emit(true)
-	if zombie_hit < 1:
-		game_end.emit(false)
--- src/Games/ZombieHunt/ZombieHuntZombie.gd
+++ src/Games/ZombieHunt/ZombieHuntZombie.gd
@@ -4,3 +4,11 @@ class_name ZombieHuntZombie
 @export var collision_shape : CollisionShape2D
 
 var game : Game
+var die : bool = false
+
+func kill():
+	queue_free()
+	get_parent().remove_child(self)
+
+func _process(_delta):
+	position.y += 1
